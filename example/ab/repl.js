02ec91d406d18cf9cf27e910fb51aa36253f66c6
0E9B061F <0E9B061F@protonmail.com>
2022-09-21 22:04:59 -0400

    update ab example

--- example/ab/repl.js
+++ example/ab/repl.js
@@ -6,12 +6,15 @@ const { inspect } = require('util')
 const parser = require('./parser.js')
 const fs = require('fs')
 const path = require('path')
+const { ast } = require('../../lib/ghast.js')
+
 const example = fs.readFileSync(path.join(__dirname, 'example.ab'), {encoding: 'utf-8'})
 
 let context
 
 const init =c=> {
   Object.assign(c, {
+    ast,
     parser,
     example,
     tree: parser(example),
@@ -23,6 +26,7 @@ function result(raw) {
 }
 
 console.log('VARIABLES')
+console.log('  ast: the ghast.js AST helper function. call to create nodes')
 console.log('  parser: ab parser')
 console.log('  example: example ab code')
 console.log('  tree: ghast.js AST as returned by calling parser(example)')
